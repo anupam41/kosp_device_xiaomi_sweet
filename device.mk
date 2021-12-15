c521261928ac49b525c732d053d314f83c8febde
someone5678 <nemui3353@gmail.com>
2021-12-15 15:05:48 +0900

    sweet: overlay: rename sweetinpro overlays and move to vendor

--- device.mk
+++ device.mk
@@ -166,8 +166,8 @@ PRODUCT_PACKAGES += \
     SettingsLibOverlaySweet \
     SettingsProviderOverlaySweet \
     SimpleDeviceConfigOverlaySweet \
-    SweetinproSettingsProvider \
-    SweetinproWifiOverlay \
+    SettingsProviderSweetinpro \
+    WifiOverlaySweetinpro \
     SystemUIOverlaySweet \
     TelephonyOverlaySweet \
     TelephonyProviderOverlaySweet \
