ArrowOS-Devices/android_device_xiaomi_sweet

sweet: Initial device tree
* Kernel (image, dtb and dtbo) and fstab from MIUI V12.0.3.0 RKFEUXM

* Architecture, kernel and dynamic partitions flags from CAF's
  BoardConfig (LA.UM.9.1.r1-09100-SMxxx0.0) except for the device
  specific values

Signed-off-by: daniml3 <danimoral1001@gmail.com>
Change-Id: Ic5adcdddc86051e8aa7ed900662e68e9a7b4bd73

AndroidProducts.mk
@@ -1,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/arrow_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    arrow_sweet-eng \
+    arrow_sweet-userdebug \
+    arrow_sweet-user

anupam41/kosp_device_xiaomi_sweet

Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

AndroidProducts.mk
@@ -1,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/nad_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    nad_sweet-eng \
+    nad_sweet-userdebug \
+    nad_sweet-user